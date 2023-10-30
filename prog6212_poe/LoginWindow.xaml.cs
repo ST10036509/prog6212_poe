713950e5af0965bc0cc8642b19fb147080601319
EthanSchoonbee <schoonbeeethan@gmail.com>
2023-10-30 19:38:25 +0200

    Created and Designed Login Page

---
+++ prog6212_poe/LoginWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace prog6212_poe
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void registerButton_Click(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void exitProgramButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
